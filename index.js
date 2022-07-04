c9586489fa4a434e8f1cca4ddedc3755584aaf5f
Diego Gaspar <88562658+gaspartv@users.noreply.github.com>
2022-07-04 08:31:21 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const arrayCompetidores = ["Rafael", "Manoel", "Daniel"]
+
 function positions(arrayCompetidores){
     let positionDaniel = arrayCompetidores.indexOf("Daniel")
     let newArrayResultado = []
     if (positionDaniel === 0) {
-        return 'Daniel é o vencedor'
+        return `Daniel é o vencedor e este é o pódio ${newArrayResultado}`
     }
     for (let i = 0; i < arrayCompetidores.length; i++) {
         if (arrayCompetidores[i+1] === "Daniel") {
             newArrayResultado.push("Daniel")
             newArrayResultado.push(arrayCompetidores[i])
             newArrayResultado.push(arrayCompetidores[i+2])
-            return 'Daniel é o vencedor'
+            return `Daniel é o vencedor e este é o pódio ${newArrayResultado}`
         } else if (arrayCompetidores[i+2] === "Daniel") {
             newArrayResultado.push(arrayCompetidores[i])
             newArrayResultado.push("Daniel")
